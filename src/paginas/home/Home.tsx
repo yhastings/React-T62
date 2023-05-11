906fc170ab176807b84b19f9ea8a430f484681cf
Liza Hainna <elizabeth.hastings121@gmail.com>
2023-05-11 15:47:21 -0300

    Primeira aula de React turma 62

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+function Home(){
+    return(
+   <>
+        <h1 className='titulo'>Hello world React</h1>
+        <img className='img' src="https://anamaria.uol.com.br/amp-stories/meu-filho-pode-assistir-round-6/assets/6.gif" alt="" />
+   </>
+   )
+}
+
+export default Home;
